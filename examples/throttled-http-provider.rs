d13909d6cb068b64748e58a0d58f8968564aa374
0xKitsune <0xKitsune@protonmail.com>
2023-11-09 11:10:37 -0500

    removed arc from example

--- examples/throttled-http-provider.rs
+++ examples/throttled-http-provider.rs
@@ -3,12 +3,10 @@ use ethers::{
     types::BlockNumber,
 };
 use ethers_throttle::ThrottledJsonRpcClient;
-use std::sync::Arc;
 use url::Url;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    // Initialize the json rpc client
     let rpc_endpoint = std::env::var("ETHEREUM_RPC_ENDPOINT")?;
     let http_provider = Http::new(Url::parse(&rpc_endpoint)?);
 
@@ -16,7 +14,7 @@ async fn main() -> eyre::Result<()> {
     let throttled_http_provider =
         ThrottledJsonRpcClient::new(http_provider, requests_per_second, None);
 
-    let middleware = Arc::new(Provider::new(throttled_http_provider));
+    let middleware = Provider::new(throttled_http_provider);
 
     let block = middleware.get_block_with_txs(BlockNumber::Latest).await?;
 
